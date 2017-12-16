ba9acd447524e20d9ceb1939d36adff646826ed6
Administrator <administrator@SOP-1482>
2017-12-16 14:18:24 -0500

    Source code etc.

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# tramp.medip
+### Project: TRAMP MeDIP-seq Data Analysis 
+### Study ID: 
+### Scientist: Wenji Li
+### Data Analysis: Davit Sargsyan
+### Created: 10/10/2017 
+
+---
+
+## Table of Contents
+[Files](#files)
+[Results](#results)   
+[technology](#tech)   
+[Daily Logs](#logs)   
+
+## Files<a name="files"></a>
+## Results <a name="results"></a>
+
+## Technology<a name="tech"></a>
+[**Methylated DNA immunoprecipitation** (**MeDIP** or **mDIP**) is a large-scale (chromosome- or genome-wide) purification technique in molecular biology that is used to enrich for methylated DNA sequences. It consists of isolating methylated DNA fragments via an antibody raised against 5-methylcytosine (5mC). This technique was first described by Weber M. et al.[1] in 2005 and has helped pave the way for viable methylome-level assessment efforts, as the purified fraction of methylated DNA can be input to high-throughput DNA detection methods such as high-resolution DNA microarrays (MeDIP-chip) or next-generation sequencing (MeDIP-seq). Nonetheless, understanding of the methylome remains rudimentary; its study is complicated by the fact that, like other epigenetic properties, patterns vary from cell-type to cell-type.](https://en.wikipedia.org/wiki/Methylated_DNA_immunoprecipitation)
+
+###Objectives
+
+## Design
+* Wild Type vs. TRAMP mice    
+* DMA methylation at 24 weeks    
+
+## Daily Logs<a name="logs"></a>
+### 08/01/2017
+* Imported data from an Excel sheet 'tramp_peaks_anno_cpg.csv', originally from an Excel file of the same name.    
+* Used variables:    
+1. **mgi_symbol**: gene name   
+2. **gene_id**: a unique region of a gene    
+3. **insideFeature**: is the location relative to genes: upstream ('promoter'), inside, or downstream ('body')    
+4. **value_1**: values from the wildtype mice at 24 weeks ('C57_24wks')    
+5. **value_2**: values from the TRAMP mice at 24 weeks ('Tramp_24wks') 
+6. **log2_fold_change**: log2(TRAMP/C57)    
+7. **description**: gene description
+
+* Remove rows if:   
+1. they are not mapped (no gene name) or    
+2. one or both values are zero or    
+3. foldcahnge is less than two    
+
+From: WENJI LI <wl365@scarletmail.rutgers.edu>
+Date: Thu, Jul 27, 2017 at 12:18 PM
+Subject: Re: TRAMP MeDIP-seq
+To: Ah-ng Kong <kongt@pharmacy.rutgers.edu>
+Cc: Davit Sargsyan <sargdavid@gmail.com>, WENJI LI <wl365@scarletmail.rutgers.edu>
+
+## From Wenji's Emails
+### 10/04/2017
+Dear Prof. Kong    
+
+As per your suggestion last time to re-submit the MeDIP-Seq ms to Cell & Bioscience, please kindly find the attached manuscript together with figures and tables, which include heatmap drawn by Davit (Fig 2) and newly improved fig 3. Thanks.   
+Best Regards,   
+Wenji    
+
+### 07/27/2017
+Dear Davit
+
+Please kindly find the attached ms draft for TRAMP MeDIP-Seq. Please let me know your available time for discussion. Thanks a lot.
+
+Best Regards,
+Wenji 
+
+---
+
+On Thu, Jul 27, 2017 at 11:41 AM, WENJI LI wrote:
+Dear Prof. Kong
+
+John pointed out some limitations in last lab meeting and we halted the re-submission to Life Sciences. I think it is a good idea to submit it to Cell & Biosciences since limitations exist in all methods. I will contact Davit about the ms. Thanks.
+
+Best Regards,
+Wenji
+
+---
+
+On Thu, Jul 27, 2017 at 11:09 AM, Ah-ng Kong wrote:
+Dear Wenji - did we go back to reanalyze the TRAMP MeDIP-seq data and may be we could send out for publication in Cell Biosciences? May be Davit can help to take a look? Tony
+
+Epigenetic alterations in TRAMP mice: genomic DNA methylation profiling using MeDIP-seq? by Wenji Li, Ying Huang, Tin Oo Khor, Guo Yue, Limin Shu, Anne Yuqing Yang, Chengyue Zhang, Michael Verzi, Ronald P Hart, and Ah-Ng Tony Kong (publication #3).
+
+like other cancers, arises from epigenetic modifications as well as genomic alterations (5, 6). We have conducted methylated DNA immunoprecipitation (MeDIP) with next- generation sequencing (MeDIP-seq) followed by Ingenuity Pathway Analysis to analyze and compare the whole genomic DNA methylation patterns between TRAMP tumors and control prostates from WT (C57BL/6) mice. The results from this study have been submitted for publication and tentatively acceptable for publication with revision in Life Sciences, ?Epigenetic alterations in TRAMP mice: genomic DNA methylation profiling using MeDIP-seq? by Wenji Li, Ying Huang, Tin Oo Khor, Guo Yue, Limin Shu, Anne Yuqing Yang, Chengyue Zhang, Michael Verzi, Ronald P Hart, and Ah-Ng Tony Kong (publication #3). The results from this study show that abnormal methylation status in CpG islands of a variety of genes in TRAMP tumors.
+
+A.-N. Tony Kong, Ph.D.
---
+++ source/tramp_medip_total_methyl_plot_v1.R
@@ -0,0 +1,201 @@
+# |----------------------------------------------------------------------------------|
+# | Project: Study of Diabetes in MES13 cells                                        |
+# | Script: Methyl-seq data analysis and visualization                               |
+# | Author: Davit Sargsyan                                                           |
+# | Created: 09/12/2017                                                              |
+# |----------------------------------------------------------------------------------|
+# Header----
+# Save consol output to a log file
+# sink(file = "tmp/log_tramp_medip_total_methyl_plot_v1.txt")
+
+require(data.table)
+require(ggplot2)
+
+# Load data----
+dt1 <- fread("data/tramp_peaks_anno_cpg-10102017.csv")
+
+# X+Y vs. X-Y
+a <- dt1$value_1 + dt1$value_2
+b <- dt1$value_1 - dt1$value_2
+plot(a ~ b)
+
+dt.in <- subset(dt1, 
+                insideFeature == "inside")
+
+tmp <- data.frame(gene = dt.in$mgi_symbol)
+
+tmp$gene_lenght_x <- dt.in$end_position.x - dt.in$start_position.x
+hist(tmp$gene_lenght_x, 100)
+
+# tmp$gene_lenght_y <- dt.in$end_position.y - dt.in$start_position.y
+# hist(tmp$gene_lenght_y, 100)
+
+tmp$pct_in_x <- dt.in$distancetoFeature/tmp$gene_lenght_x
+hist(tmp$pct_in_x)
+
+tmp$pct_in_y <- dt.in$distancetoFeature/tmp$gene_lenght_y
+hist(tmp$pct_in_y)
+
+tmp$val1 <- dt.in$value_1
+tmp$val2 <- dt.in$value_2
+
+summary(tmp)
+
+plot(tmp$val2 ~ tmp$pct_in_x)
+points(tmp$val1 ~ tmp$pct_in_x,
+       col = "red")
+
+m1 <- loess(tmp$val1 ~ tmp$pct_in_x, span = 0.3)
+m2 <- loess(tmp$val2 ~ tmp$pct_in_x, span = 0.3)
+dt2 <- data.table(x = tmp$pct_in_x,
+                  y1 = m1$fitted,
+                  y2 = m2$fitted)
+dt2 <- dt2[order(dt2$x)]
+plot(dt2$y2 ~ dt2$x, type = "l", ylim = c(0, 25))
+lines(dt2$y1 ~ dt2$x, col = "red")
+
+# Upstream (-3kb?)----
+unique(dt1$insideFeature)
+dt.up <- subset(dt1, 
+                insideFeature == "upstream")
+summary(dt.up$distancetoFeature)
+table(dt.up$distancetoFeature >= -3000)
+length(unique(dt.up$mgi_symbol))
+dt.up <- droplevels(subset(dt.up,
+                           distancetoFeature >= -3000))
+
+tmp1 <- data.frame(gene = dt.up$mgi_symbol,
+                   gene_lenght_x = NA)
+
+tmp1$pct_in_x <- dt.up$distancetoFeature/3000
+hist(tmp1$pct_in_x)
+
+tmp1$val1 <- dt.up$value_1
+tmp1$val2 <- dt.up$value_2
+
+summary(tmp1)
+
+plot(tmp1$val2 ~ tmp1$pct_in_x)
+points(tmp1$val1 ~ tmp1$pct_in_x,
+       col = "red")
+
+m1 <- loess(tmp1$val1 ~ tmp1$pct_in_x, span = 0.3)
+m2 <- loess(tmp1$val2 ~ tmp1$pct_in_x, span = 0.3)
+dt3 <- data.table(x = tmp1$pct_in_x,
+                  y1 = m1$fitted,
+                  y2 = m2$fitted)
+dt3 <- dt3[order(dt3$x)]
+plot(dt3$y2 ~ dt3$x, type = "l")
+lines(dt3$y1 ~ dt3$x, col = "red")
+
+# Downstream (3kb?)----
+dt.dn <- subset(dt1, 
+                insideFeature == "downstream")
+summary(dt.dn$distancetoFeature)
+table(dt.dn$distancetoFeature <= 3000)
+
+dt.dn <- droplevels(subset(dt.dn,
+                           distancetoFeature <= 3000))
+
+tmp2 <- data.frame(gene = dt.dn$mgi_symbol,
+                   gene_lenght_x = NA)
+
+tmp2$pct_in_x <- dt.dn$distancetoFeature/3000 + 1
+hist(tmp2$pct_in_x)
+
+tmp2$val1 <- dt.dn$value_1
+tmp2$val2 <- dt.dn$value_2
+
+summary(tmp2)
+
+plot(tmp2$val2 ~ tmp2$pct_in_x)
+points(tmp2$val1 ~ tmp2$pct_in_x,
+       col = "red")
+
+m1 <- loess(tmp2$val1 ~ tmp2$pct_in_x, span = 0.3)
+m2 <- loess(tmp2$val2 ~ tmp2$pct_in_x, span = 0.3)
+dt4 <- data.table(x = tmp2$pct_in_x,
+                  y1 = m1$fitted,
+                  y2 = m2$fitted)
+dt4 <- dt4[order(dt4$x)]
+plot(dt4$y2 ~ dt4$x, type = "l")
+lines(dt4$y1 ~ dt4$x, col = "red")
+
+# Combine----
+dt.all <- rbind(tmp, tmp1, tmp2)
+plot(log2(dt.all$val2 + 1) ~ dt.all$pct_in_x,
+     main = "Methylation (%) By Location",
+     xaxt = "none",
+     xlab = "Location",
+     ylab = "log2(Readout + 1)")
+points(log2(dt.all$val1 + 1) ~ dt.all$pct_in_x,
+       col = "red")
+axis(side = 1,
+     at = -1:2,
+     labels = c("3kb Upstream",
+                "TSS",
+                "TTS",
+                "3kb Downstream"))
+
+m1 <- loess(dt.all$val1 ~ dt.all$pct_in_x, span = 0.25)
+m2 <- loess(dt.all$val2 ~ dt.all$pct_in_x, span = 0.25)
+dt5 <- data.table(x = dt.all$pct_in_x,
+                  y1 = m1$fitted,
+                  y2 = m2$fitted)
+ggData <- dt5[order(dt5$x)]
+
+
+ggData <- melt.data.table(data = ggData,
+                          id.vars = "x",
+                          measure.vars = c("y1", "y2"),
+                          variable.name = "Treatment",
+                          value.name = "Methylation(%)")
+ggData$Treatment <- factor(ggData$Treatment,
+                           levels = c("y1", 
+                                      "y2"),
+                           labels = c("C57 at 24 Weeks",
+                                      "TRAMP at 24 Weeks"))
+
+ggplot(ggData,
+       aes(x = x,
+           y = `Methylation(%)`,
+           group = Treatment,
+           colour = Treatment)) +
+  geom_line() +
+  scale_x_continuous("Region",
+                     breaks = -1:2,
+                     labels = c("3kb Upstream",
+                                "TSS",
+                                "TTS",
+                                "3kb Downstream"))
+
+# Remove zeros----
+summary(dt.all)
+m1 <- loess(dt.all$val1[dt.all$val1 > 0] ~ dt.all$pct_in_x[dt.all$val1 > 0], 
+            span = 0.5)
+m2 <- loess(dt.all$val2[dt.all$val2 > 0] ~ dt.all$pct_in_x[dt.all$val2 > 0],
+            span = 0.5)
+dt5 <- rbindlist(list(data.table(x = m1$x,
+                                 y = m1$fitted,
+                                 grp = 1),
+                      (data.table(x = m2$x,
+                                  y = m2$fitted,
+                                  grp = 2))))
+names(dt5) <- c("x", "y", "grp")
+dt5$grp <- factor(dt5$grp, 
+                  levels = 1:2,
+                  labels = c("C57 at 24 Weeks",
+                             "TRAMP at 24 Weeks"))
+dt5
+ggplot(dt5,
+       aes(x = x,
+           y = y,
+           group = grp,
+           colour = grp)) +
+  geom_line() +
+  scale_x_continuous("Region",
+                     breaks = -1:2,
+                     labels = c("3kb Upstream",
+                                "TSS",
+                                "TTS",
+                                "3kb Downstream"))
